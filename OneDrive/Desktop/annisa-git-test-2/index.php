10f536019ab372843f905b8555cf585f4ce37c9b
hakimlearn <annisahakim.learn@gmail.com>
2026-04-24 11:44:04 +0700

    menambahkan list

--- OneDrive/Desktop/annisa-git-test-2/index.php
+++ OneDrive/Desktop/annisa-git-test-2/index.php
@@ -8,6 +8,6 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque inventore impedit voluptate temporibus consequatur unde, obcaecati quibusdam sed sapiente, neque consequuntur dolore accusantium veritatis nulla dicta. Deleniti dignissimos maxime modi?</p>
-    
+    <li>ya</li>
 </body>
 </html>
